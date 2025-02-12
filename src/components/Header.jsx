d766687d922f0ffb64ccb01cc02620dce0beb38d
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-12 16:28:28 +0100

    create css header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,21 @@ export default function Header() {
     return (
         <header className="header">
             <div className="logo">
-                <img src="../../public/img/dc-logo.png" alt="DC Comics Logo" />
+                <img src="/img/dc-logo.png" alt="DC Comics Logo" />
             </div>
             <nav>
                 <ul>
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
                     <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Collectibles</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">Fans</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+
                 </ul>
             </nav>
         </header>
