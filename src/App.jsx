71aef54c39f2a48338193be4fbdaa1e9ca06a67d
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-12 15:55:08 +0100

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import './App.css'
+// importa componenti
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
+
+// import './App.css'
 
 function App() {
 
   return (
     <>
+      <Header />
+      <Main />
+      <Footer />
 
     </>
   )
