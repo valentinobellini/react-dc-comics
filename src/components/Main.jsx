03fe5696623aa72fefcb5684fb3fa0f99b9466a8
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-12 22:29:58 +0100

    create banner component BONUS

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,7 @@
+// importa componenti
+import ServicesBanner from "./banners/ServicesBanner";
+
+
 // crea componente main.jsx
 export default function Main() {
     return (<main className="main-content">
@@ -6,30 +10,7 @@ export default function Main() {
                 <h2> Content goes here </h2>
             </section>
         </div>
-        <div className="services-wrapper">
-            <section className="services">
-                <div className="service">
-                    <img className="services-img" src="img/buy-comics-digital-comics.png" alt="" />
-                    <p>DIGITAL COMICS</p>
-                </div>
-                <div className="service">
-                    <img className="services-img" src="img/buy-comics-merchandise.png" alt="" />
-                    <p>DC MERCHANDISE</p>
-                </div>
-                <div className="service">
-                    <img className="services-img" src="img/buy-comics-subscriptions.png" alt="" />
-                    <p>SUBSCRIPTION</p>
-                </div>
-                <div className="service">
-                    <img className="services-img" src="img/buy-comics-shop-locator.png" alt="" />
-                    <p>COMIC SHOP LOCATOR</p>
-                </div>
-                <div className="service">
-                    <img className="services-img" src="img/buy-dc-power-visa.svg" alt="" />
-                    <p>DC POWER VISA</p>
-                </div>
-            </section>
-        </div>
+        <ServicesBanner />
     </main>
     );
 };
---
+++ src/components/banners/ServicesBanner.jsx
@@ -0,0 +1,28 @@
+export default function ServicesBanner() {
+    return (
+        <div className="services-wrapper">
+            <section className="services">
+                <div className="service">
+                    <img className="services-img" src="img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+                </div>
+                <div className="service">
+                    <img className="services-img" src="img/buy-comics-merchandise.png" alt="" />
+                    <p>DC MERCHANDISE</p>
+                </div>
+                <div className="service">
+                    <img className="services-img" src="img/buy-comics-subscriptions.png" alt="" />
+                    <p>SUBSCRIPTION</p>
+                </div>
+                <div className="service">
+                    <img className="services-img" src="img/buy-comics-shop-locator.png" alt="" />
+                    <p>COMIC SHOP LOCATOR</p>
+                </div>
+                <div className="service">
+                    <img className="services-img" src="img/buy-dc-power-visa.svg" alt="" />
+                    <p>DC POWER VISA</p>
+                </div>
+            </section>
+        </div>
+    );
+};
