5966bddae356e515f22794fc032e3f8b4047de02
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-13 14:54:31 +0100

    import NavBar component in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+// importa componenti
+import NavBar from "./NavBar";
+
 // crea componente header.jsx
 export default function Header() {
     return (
@@ -6,19 +9,7 @@ export default function Header() {
                 <img src="/img/dc-logo.png" alt="DC Comics Logo" />
             </div>
             <nav>
-                <ul>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#">COMICS</a></li>
-                    <li><a href="#">MOVIES</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">GAMES</a></li>
-                    <li><a href="#">COLLECTIBLES</a></li>
-                    <li><a href="#">VIDEOS</a></li>
-                    <li><a href="#">FANS</a></li>
-                    <li><a href="#">NEWS</a></li>
-                    <li><a href="#">SHOP</a></li>
-
-                </ul>
+                <NavBar />
             </nav>
         </header>
     );
