cdf5bac63e9fb092443cc690b689fac74a62be6d
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-13 15:27:54 +0100

    use NavBar links as props to NavBar in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,28 @@ import NavBar from "./NavBar";
 
 // crea componente header.jsx
 export default function Header() {
+
+    // array oggetti navLinks
+    const navLinks = [
+        { id: 1, name: "CHARACTERS", url: "#", current: false },
+        { id: 2, name: "COMICS", url: "#", current: false },
+        { id: 3, name: "MOVIES", url: "#", current: true },
+        { id: 4, name: "TV", url: "#", current: false },
+        { id: 5, name: "GAMES", url: "#", current: false },
+        { id: 6, name: "COLLECTIBLES", url: "#", current: false },
+        { id: 7, name: "VIDEOS", url: "#", current: false },
+        { id: 8, name: "FANS", url: "#", current: false },
+        { id: 9, name: "NEWS", url: "#", current: false },
+        { id: 10, name: "SHOP", url: "#", current: false }
+    ];
+
     return (
         <header className="header">
             <div className="logo">
                 <img src="/img/dc-logo.png" alt="DC Comics Logo" />
             </div>
             <nav>
-                <NavBar />
+                <NavBar linksProp={navLinks} />
             </nav>
         </header>
     );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,7 @@
-// crea array oggetti navLinks
-const navLinks = [
-    { id: 1, name: "CHARACTERS", url: "#", current: false },
-    { id: 2, name: "COMICS", url: "#", current: false },
-    { id: 3, name: "MOVIES", url: "#", current: true },
-    { id: 4, name: "TV", url: "#", current: false },
-    { id: 5, name: "GAMES", url: "#", current: false },
-    { id: 6, name: "COLLECTIBLES", url: "#", current: false },
-    { id: 7, name: "VIDEOS", url: "#", current: false },
-    { id: 8, name: "FANS", url: "#", current: false },
-    { id: 9, name: "NEWS", url: "#", current: false },
-    { id: 10, name: "SHOP", url: "#", current: false }
-];
-
-
-export default function NavBar() {
+export default function NavBar(props) {
     return (
         <ul>
-            {navLinks.map((link) => (
+            {props.linksProp.map((link) => (
                 <li key={link.id}>
                     <a href={link.url}>
                         {link.name}
